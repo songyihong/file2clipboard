8384c7ea613d1452e1a785485dbfe6573836ebe2
Dmytro Belyaew <sharpfuryz@gmail.com>
2018-01-09 02:47:04 -0800

    Initial commit! Usage: file2clipboard.exe absolute_file_path File will be available in clipboard, after app closes.

---
+++ file2clipboard/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace file2clipboard
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Application.Run();
+            copyFile(args);
+            Application.Exit();
+        }
+
+        private static void copyFile(string[] args)
+        {
+            StringCollection filePaths = new StringCollection();
+            if (args.Length > 0)
+            {
+                String filename = args[0];
+                filePaths.Add(filename);
+                Clipboard.Clear();
+                Clipboard.SetFileDropList(filePaths);
+            }
+        }
+    }
+}
